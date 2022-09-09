c486e3b50d6e7cabc18c62afe7a60c965c51721b
take44444 <take44444.general@gmail.com>
2022-09-09 10:54:31 +0900

    fix bug of compile command

--- docker/bin/compile.sh
+++ docker/bin/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '[info]  Compiling your L4T code.'
-/usr/local/bin/l4tc < "$@" 1> ./out.S
+/usr/local/bin/l4tc 1> ./out.S
 if [ $? -gt 0 ]; then
   echo '[error] Failed to compile your code. Please check the error message above.' >&2
   exit 1
